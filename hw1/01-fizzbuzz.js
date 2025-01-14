f02ca4660fa7ccc11bbd4dd88255786cc1684767
planetaska <planetaska@gmail.com>
2025-01-14 14:37:25 -0800

    Polish code comment by style guide

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -10,8 +10,10 @@
  **/
 
 const fizzbuzz = () => {
-  // Loop through 1 to 100 and print values accordingly,
-  // as instructed by the homework assignment.
+  /**
+   * Loop through 1 to 100 and print values accordingly,
+   * as instructed by the homework assignment.
+   */
   for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log('fizzbuzz');
