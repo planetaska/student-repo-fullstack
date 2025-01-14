eebc4c5b5e25070850490fe3bcf4e36022f00dc5
planetaska <planetaska@gmail.com>
2025-01-14 14:10:01 -0800

    Implement fizzbuzz function

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -1,32 +1,27 @@
 /** Exercise 01 - Fizzbuzz
 
-Write a program that writes all the numbers from 1 to 100, with some exceptions: 
-- For numbers divisible by 3, print “fizz” 
-- For numbers divisible by 5 (but not 3), print “buzz” 
-- For numbers divisible by 3 and 5, print “fizzbuzz”
+ Write a program that writes all the numbers from 1 to 100, with some exceptions:
+ - For numbers divisible by 3, print “fizz”
+ - For numbers divisible by 5 (but not 3), print “buzz”
+ - For numbers divisible by 3 and 5, print “fizzbuzz”
 
-Use console.log() to write the proper output to the command line.
+ Use console.log() to write the proper output to the command line.
 
-**/
+ **/
 
 const fizzbuzz = () => {
-  // Add your code here
+  // Loop through 1 to 100 and print values as requested by the homework
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
 };
 
 fizzbuzz();
-// 1
-// 2
-// fizz
-// 4
-// buzz
-// fizz
-// 7
-// 8
-// fizz
-// buzz
-// 11
-// fizz
-// 13
-// 14
-// fizzbuzz
-// ...
